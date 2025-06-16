b66dced479ad4bc65e8fc8b525e8e73ca9b79a21
Kean <kmenendez@estudiante.edib.es>
2025-06-16 13:09:17 +0200

    datos project

---
+++ app/Models/Project.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+     protected $fillable = [
+        'id',
+        'name', 
+        'description',  
+        'color_project', 
+        'due_date', 
+        'image', 
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $table='project';
+
+    protected $primaryKey='id';
+
+    protected $casts=[
+        'due_date'=>'date'
+    ];
+
+    public function users(){
+        return $this->belongsToMany(User::class,'project_user','project_id','user_id');
+    }
+
+}
